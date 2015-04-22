3e34c7452e02749d0fa2638c2b4abb7c8f47e8fb
jokesterfr <main@jokester.fr>
2015-04-22 15:53:02 +0200

    Bump v0.1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
       default: {
         files: {
           'dist/browser-component.html': 'lib/browser-component.html'
-        },
+        }
       },
     },
 
